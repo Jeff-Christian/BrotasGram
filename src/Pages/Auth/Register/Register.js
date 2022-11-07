6f51d1d16b639da601b69776316ca47befe10147
Jeff Christian <jeffchristian.devfrontend@gmail.com>
2022-11-07 19:17:14 -0300

    inicialized intregation with backend in register form, added redux methods

--- src/Pages/Auth/Register/Register.js
+++ src/Pages/Auth/Register/Register.js
@@ -5,12 +5,27 @@ import { Link } from "react-router-dom";
 
 // Images
 import logo from "../../../Images/brotasGram-logo.svg";
+import { useState } from "react";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-  }
+
+    const user = {
+      name, 
+      email,
+      password, 
+      confirmPassword
+    }
+
+    console.log(user);
+
+  };
 
   return (
 
@@ -24,10 +39,39 @@ const Register = () => {
         </p>
 
         <form onSubmit={handleSubmit}>
-          <input type="text" name="email" id="email" placeholder="Email" />
-          <input type="text" name="userName" id="userName" placeholder="Nome de usuário" />
-          <input type="password" name="password" id="password" placeholder="Senha" />
-          <input type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirme sua senha" />
+
+          <input 
+          type="text" 
+          name="email" 
+          id="email" 
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          placeholder="Email" />
+
+          <input 
+          type="text" 
+          name="userName" 
+          id="userName" 
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+          placeholder="Nome de usuário" />
+
+          <input 
+          type="password" 
+          name="password" 
+          id="password" 
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          placeholder="Senha" />
+
+          <input 
+          type="password" 
+          name="confirmPassword" 
+          id="confirmPassword" 
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          value={confirmPassword}
+          placeholder="Confirme sua senha" />
+
           <p>
             Ao se cadastrar você concorda com os nossos Termos, Privacidade, Politícas de uso e Cookies.
           </p>
